b70828f621c920e443e3144e7ef7d96a9dd781d2
Jorge <alfred.jorge@gmail.com>
2022-12-21 18:49:20 -0300

    Average rating by day week

---
+++ review_analysis/6-av-rat-dayweek.py
@@ -0,0 +1,88 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+import matplotlib.pyplot as plt
+
+data= pandas.read_csv("reviews.csv", parse_dates=["Timestamp"])
+# A= days of the week
+data["Weekday"] = data["Timestamp"].dt.strftime("%A")
+# w= number of days of the week 0 to 6
+data["Daynumber"] = data["Timestamp"].dt.strftime("%w")
+
+weekday_average = data.groupby(["Weekday", "Daynumber"]).mean()
+weekday_average = weekday_average.sort_values("Daynumber")
+
+chart_def = """
+{
+    chart: {
+        type: 'spline',
+        inverted: false # true = (y,x)
+    },
+    title: {
+        text: 'Atmosphere Temperature by Altitude'
+    },
+    subtitle: {
+        text: 'Happiest Day of the Week'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Day'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 80 km.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Average Rating'
+        },
+        labels: {
+            format: '{value}'
+        },
+        accessibility: {
+            rangeDescription: 'Range: -90°C to 20°C.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x}: {point.y}'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Average Rating',
+        data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+    }]
+}
+"""
+def app():
+    wp = jp.QuasarPage()
+    # Quasar typography for more styles
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h2 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis", classes="text-center")
+    hc = jp.HighCharts(a=wp, options=chart_def)
+    hc.options.title.text = "Average Rating by Day"
+
+    hc.options.xAxis.categories = list(weekday_average.index.get_level_values(0))
+    hc.options.series[0].data = list(weekday_average["Rating"])
+    return wp
+
+jp.justpy(app)
